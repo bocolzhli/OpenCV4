6c31b31c92f6c162404aa8ad892fcd46b79621ac
bocol <bocol@outlook.com>
2022-03-19 10:10:35 +0800

    project creation

---
+++ opencv4/lib/core/helpers.dart
@@ -0,0 +1,28 @@
+// @dart=2.9
+/*
+ * flutter_opencv
+ * https://mulgundkar.com
+ * 
+ * Copyright (c) 2020 Aditya Mulgundkar. All rights reserved.
+ * See LICENSE for more details.
+ */
+
+/// Helper Size class with width & height doubles
+class Size {
+  double x;
+  double y;
+  Size(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+/// Helper Point class with x & y axis doubles
+class Point {
+  double x;
+  double y;
+  Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+}
